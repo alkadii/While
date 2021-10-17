96d59448ee2316e5497be40698f1d22644a85720
khaled <alkadi.khaled@hotmail.com>
2021-10-17 14:51:25 +0200

    1

---
+++ Test while/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (false)
+            {
+                Console.WriteLine(100);
+                
+
+            }
+
+
+        }
+    }
+}
