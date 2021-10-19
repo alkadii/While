1167b021289ab4c46b19512aa7a01d30bb59555a
khaled <alkadi.khaled@hotmail.com>
2021-10-19 13:25:56 +0200

    2

--- Test while/Program.cs
+++ Test while/Program.cs
@@ -10,14 +10,85 @@ namespace Test_while
     {
         static void Main(string[] args)
         {
-            while (true)
-            {
-                Console.WriteLine(100);
-                
+            #region لووب لانهائية
+            //while (true)
+            //{
+            //    Console.WriteLine(100);
 
-            }
+            //    int y = 0;
+            //    while (true)
+            //    {
+            //        Console.WriteLine(y);
+            //        y++;
+            //    }
+            //}
+
+
+
+            //int x = 10;
+            //    while (x >= 0)
+            #endregion
+
+            #region while mit IF
+
+
+            //int f = 0;
+            //while (true)
+            //{
+            //    Console.WriteLine(f);
+            //    f++;
+            //    if (f >100) break;
+
+            //}
+            #endregion
+
+            #region While mit Do , do{....} while(Vorussetzung); 
+            // for und Do-while الفرق هو ان DO-while تبدأ طباعة قبل الشرط
+
+            //do
+            //{
+            //    Console.Write(1);
+            //}
+            //while (true);
+
+            //int a = 0;
+            //do
+            //{
+            //    Console.WriteLine(a);
+
+            //    a++;
+            //}
+            //while (a<=200);
+
+
+
+
+
+            #endregion
+
+            #region While - Beispiel
+
+            string YN ;
+            do
+            {
+                Console.Write("Erste Nummer: ");
+                int ers = int.Parse(Console.ReadLine());
+                Console.Write("Zweite Nummer: ");
+                int zwei = int.Parse(Console.ReadLine());
+                int summ = ers + zwei;
+                Console.WriteLine("=========");
+                Console.WriteLine(summ);
+                Console.Write("Möchten Sie wiederholen (Y,N): ");
+                YN = Console.ReadLine();
+            } while (YN == "Y" || YN == "y");
+
+
+            #endregion
+
+
 
 
         }
+
     }
 }
